dcea794159491b536f4f9584b0f370f285fc9a2a
Joanne Notley <49209412+jo1807@users.noreply.github.com>
2025-04-04 18:14:00 +0100

    add data

---
+++ app/utils/data.ts
@@ -0,0 +1,58 @@
+export const investmentData = [
+  {
+    id: 1,
+    name: "Growth Funds",
+    data: [
+      {
+        name: "VT Element Cautious",
+        rating: { analystRating: 4, SRRI: 4 },
+        portfolio: [
+          { label: "Stock", value: 25.53738 },
+          { label: "Bond", value: 54.25369 },
+          { label: "Cash", value: 19.69435 },
+          { label: "Other", value: 1.00772 },
+          { label: "Property", value: 0 },
+        ],
+      },
+      {
+        name: "VT Element Balanced",
+        rating: { analystRating: 3, SRRI: 2 },
+        portfolio: [
+          { label: "Stock", value: 55.82681 },
+          { label: "Bond", value: 35.29226 },
+          { label: "Cash", value: 8.6057 },
+          { label: "Other", value: 1.48948 },
+          { label: "Property", value: 0 },
+        ],
+      },
+      {
+        name: "VT Element Adventurous",
+        rating: { analystRating: 3, SRRI: 1 },
+        portfolio: [
+          { label: "Stock", value: 85.0684 },
+          { label: "Bond", value: 11.11107 },
+          { label: "Cash", value: 3.65583 },
+          { label: "Other", value: 1.52162 },
+          { label: "Property", value: 0 },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Responsible Fund",
+    data: [
+      {
+        name: "VT Element Responsible Gr",
+        rating: { analystRating: 4, SRRI: 5 },
+        portfolio: [
+          { label: "Stock", value: 85.27585 },
+          { label: "Bond", value: 10.89832 },
+          { label: "Cash", value: 4.2768 },
+          { label: "Other", value: 1.48467 },
+          { label: "Property", value: 0 },
+        ],
+      },
+    ],
+  },
+];
